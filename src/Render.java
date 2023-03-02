dfeee4ace6da46627d987daa815adcda42cbf5b3
nikthegamer <sam.zap9@gmail.com>
2023-03-02 16:54:23 +0100

    Added explanation to code, organized some code

--- src/Render.java
+++ src/Render.java
@@ -3,11 +3,49 @@ import org.lwjgl.input.Keyboard;
 import static org.lwjgl.opengl.GL11.*;
 
 public class Render {
+    //---DRAW RAYS---
+
+    private final double PI = Math.PI;
+    private final double P2 = PI / 2;
+    private final double P3 = 3 * PI / 2;
+    private final float DR = 0.0174533f; //1 degree in radians
+    //---RENDER PLAYER---
+    private final int mapX = 8;
+    private final int mapY = 8;
+    private final int mapS = 64;
+    int[] mapW = {
+            2, 2, 7, 2, 2, 5, 2, 2,
+            6, 0, 0, 0, 2, 0, 0, 2,
+            2, 0, 0, 0, 4, 0, 0, 3,
+            2, 2, 4, 2, 5, 2, 4, 2,
+            2, 0, 0, 0, 0, 0, 0, 2,
+            3, 0, 0, 0, 0, 0, 0, 3,
+            2, 0, 0, 0, 0, 0, 0, 2,
+            2, 2, 2, 3, 6, 2, 2, 2
+    };
+    int[] mapF = {
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 1, 0, 0, 0,
+            0, 0, 0, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 1, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0
+    };
+    int[] mapC = {
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0
+    };
     private float px, py, pdx, pdy, pa;
-    private double PI = Math.PI;
-    private double P2 = PI / 2;
-    private double P3 = 3 * PI / 2;
-    private float DR = 0.0174533f; //1 degree in radians
+    private int r, mx, my, mp, dof;
+    private float rx, ry, ra, xo, yo, disT, shade = 1;
 
     public void setData(float pdx, float pdy, float px, float py) {
         this.pdx = pdx;
@@ -28,68 +66,21 @@ public class Render {
         return (float) Math.sin(a);
     }
 
-    float FixAng(float a) {
-        if (a > 359) {
-            a -= 360;
-        }
-        if (a < 0) {
-            a += 360;
-        }
-        return a;
-    }
-
-    float RadtoDeg(float a) {
-        return (float) (a * 180 / PI);
-    }
-
     public void game() {
         buttons();
-        drawMap2D();
-        drawPlayer();
-    }
-
-    //---RENDER PLAYER---
-
-    void drawPlayer() {
-        glColor3f(1, 1, 0);
-        glPointSize(8);
-        glBegin(GL_POINTS);
-        glVertex2f(px, py);
-        glEnd();
         drawRays3D();
     }
 
-    //---DRAW RAYS---
-    Textures textures = new Textures();
-    int[] All_textures = textures.All_Textures;
-
-    private int r, mx, my, mp, dof;
-    private float rx, ry, ra, xo, yo, disT, shade = 1;
+    //---RENDER MAP----
 
     void drawRays3D() {
-
-        glColor3f(0, 1, 1);
-        glBegin(GL_QUADS);
-        glVertex2i(526, 0);
-        glVertex2i(1006, 0);
-        glVertex2i(1006, 160);
-        glVertex2i(526, 160);
-        glEnd();
-        glColor3f(0, 0, 1);
-        glBegin(GL_QUADS);
-        glVertex2i(526, 160);
-        glVertex2i(1006, 160);
-        glVertex2i(1006, 320);
-        glVertex2i(526, 320);
-        glEnd();
-
         ra = pa - DR * 30;
         if (ra < 0) {
             ra += 2 * PI;
         } else if (ra > 2 * PI) {
             ra -= 2 * PI;
         }
-        for (r = 0; r < 60; r++) //Num of rays
+        for (r = 0; r < 120; r++) //Num of rays
         {
             int vmt = 0, hmt = 0;
             dof = 0;
@@ -182,21 +173,13 @@ public class Render {
                 ry = vy;
                 disT = disV;
                 shade = 1f;
-                glColor3f(0.9f, 0, 0);
             }
             if (disH < disV) {
                 rx = hx;
                 ry = hy;
                 disT = disH;
                 shade = 0.5f;
-                glColor3f(0.7f, 0, 0);
             }
-
-            glLineWidth(1);
-            glBegin(GL_LINES);
-            glVertex2f(px, py);
-            glVertex2f(rx, ry);
-            glEnd();
 
             float ca = pa - ra;
             if (ca < 0) {
@@ -206,21 +189,24 @@ public class Render {
                 ca -= 2 * PI;
             }
             disT = (float) (disT * Math.cos(ca)); //Fix fisheye
-            float lineH = (mapS * 320) / disT; //Line height
+            float lineH = (mapS * 640) / disT; //Line height
 
             float ty_step = 32.0f / lineH;
             float ty_off = 0;
 
-            if (lineH > 320) {
-                ty_off = (lineH - 320) / 2.0f;
-                lineH = 320;
+            if (lineH > 640) {
+                ty_off = (lineH - 640) / 2.0f;
+                lineH = 640;
             }
-            float lineO = 160 - ((int) lineH >> 1); //Line offset
+            float lineO = 320 - ((int) lineH >> 1); //Line offset
 
 
             //---Draw walls---
-            float ty = ty_off * ty_step + hmt * 32;
+            int y;
+            float ty = ty_off * ty_step; //+ hmt * 32;
             float tx;
+            int red = 0, green = 0, blue = 0;
+
 
             if (shade == 1) {
                 tx = (int) ((ry / 2.0f) % 32);
@@ -234,54 +220,82 @@ public class Render {
                 }
             }
 
-            int y;
             for (y = 0; y < lineH; y++) {
-                float c = All_textures[((int) (ty) * 32 + (int) (tx))%4096] * shade;
+                int pixel = ((int) ty * 32 + (int) tx) * 3 + (hmt * 32 * 32 * 3);
                 if (hmt == 0) {
-                    glColor3f(c, c / 2, c / 2);
-                } //checkerboard
+                    red = (int) (Textures.Missing_Texture[(pixel) % 3072] * shade);
+                    green = (int) (Textures.Missing_Texture[(pixel + 1) % 3072] * shade);
+                    blue = (int) (Textures.Missing_Texture[(pixel + 2) % 3072] * shade);
+                }
                 if (hmt == 1) {
-                    glColor3f(0, 0, c/2);
-                } //bricks
+                    red = (int) (Textures.houseWall[(pixel) % 3072] * shade);
+                    green = (int) (Textures.houseWall[(pixel + 1) % 3072] * shade);
+                    blue = (int) (Textures.houseWall[(pixel + 2) % 3072] * shade);
+                }
                 if (hmt == 2) {
-                    glColor3f(c / 2, c / 2, c);
-                } //window
+                    red = (int) (Textures.houseWindow[(pixel) % 3072] * shade);
+                    green = (int) (Textures.houseWindow[(pixel + 1) % 3072] * shade);
+                    blue = (int) (Textures.houseWindow[(pixel + 2) % 3072] * shade);
+                }
                 if (hmt == 3) {
-                    glColor3f(c / 2, c, c / 2);
-                } //door
+                    red = (int) (Textures.houseDoor[(pixel) % 3072] * shade);
+                    green = (int) (Textures.houseDoor[(pixel + 1) % 3072] * shade);
+                    blue = (int) (Textures.houseDoor[(pixel + 2) % 3072] * shade);
+                }
+                if (hmt == 4) {
+                    red = (int) (Textures.houseDecoOne[(pixel) % 3072] * shade);
+                    green = (int) (Textures.houseDecoOne[(pixel + 1) % 3072] * shade);
+                    blue = (int) (Textures.houseDecoOne[(pixel + 2) % 3072] * shade);
+                }
+                if (hmt == 5) {
+                    red = (int) (Textures.houseDecoTwo[(pixel) % 3072] * shade);
+                    green = (int) (Textures.houseDecoTwo[(pixel + 1) % 3072] * shade);
+                    blue = (int) (Textures.houseDecoTwo[(pixel + 2) % 3072] * shade);
+                }
+                if (hmt == 6) {
+                    red = (int) (Textures.houseDoor[(pixel) % 3072] * shade);
+                    green = (int) (Textures.houseDoor[(pixel + 1) % 3072] * shade);
+                    blue = (int) (Textures.houseDoor[(pixel + 2) % 3072] * shade);
+                }
                 glPointSize(8);
+                glColor3ub((byte) red, (byte) green, (byte) blue);
                 glBegin(GL_POINTS);
-                glVertex2f(r * 8 + 530, y + lineO);
+                glVertex2f(r * 8, y + lineO);
                 glEnd();
                 ty += ty_step;
             }
             //---Draw floor---
-            for (y = (int) (lineO + lineH); y < 320; y++) {
-                float dy = y - (320.0f / 2.0f);
+            for (y = (int) (lineO + lineH); y < 640; y++) {
+                float dy = y - (640.0f / 2.0f);
                 float raFix = cos(pa - ra);
-                tx = px / 2 + cos(ra) * 158 * 32 / dy / raFix;
-                ty = py / 2 + sin(ra) * 158 * 32 / dy / raFix;
+                tx = px / 2 + cos(ra) * 158 * 2 * 32 / dy / raFix;
+                ty = py / 2 + sin(ra) * 158 * 2 * 32 / dy / raFix;
 
                 int mp = mapF[(int) (ty / 32.0f) * mapX + (int) (tx / 32.0f)] * 32 * 32;
-
-                float c = All_textures[((int) (ty) & 31) * 32 + ((int) (tx) & 31) + mp] * 0.7f;
-                glColor3f(c, c, c);
+                int pixel = (((int) (ty) & 31) * 32 + ((int) (tx) & 31)) * 3 + mp * 3;
+                red = Textures.houseFloor[(pixel) % 3072];
+                green = Textures.houseFloor[(pixel + 1) % 3072];
+                blue = Textures.houseFloor[(pixel + 2) % 3072];
                 glPointSize(8);
+                glColor3ub((byte) red, (byte) green, (byte) blue);
                 glBegin(GL_POINTS);
-                glVertex2f(r * 8 + 530, y);
+                glVertex2f(r * 8, y);
                 glEnd();
 
                 //draw ceiling
                 mp = mapC[(int) (ty / 32.0f) * mapX + (int) (tx / 32.0f)] * 32 * 32;
-                c = All_textures[((int) (ty) & 31) * 32 + ((int) (tx) & 31) + mp] * 0.7f;
-                glColor3f(c, c, c);
+                pixel = (((int) (ty) & 31) * 32 + ((int) (tx) & 31)) * 3 + mp * 3;
+                red = Textures.houseCeiling[(pixel) % 3072];
+                green = Textures.houseCeiling[(pixel + 1) % 3072];
+                blue = Textures.houseCeiling[(pixel + 2) % 3072];
                 glPointSize(8);
+                glColor3ub((byte) (red * 0.7), (byte) (green * 0.7), (byte) (blue * 0.7));
                 glBegin(GL_POINTS);
-                glVertex2f(r * 8 + 530, 320 - y);
+                glVertex2f(r * 8, 640 - y);
                 glEnd();
             }
 
-            ra += DR;
+            ra += DR / 2;
             if (ra < 0) {
                 ra += 2 * PI;
             } else if (ra > 2 * PI) {
@@ -299,6 +313,7 @@ public class Render {
     }
 
     void buttons() {
+
         if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
             pa -= 0.1;
             if (pa < 0) {
@@ -354,63 +369,19 @@ public class Render {
             if (mapW[ipy_add_yo * mapX + ipx_add_xo] == 4) {
                 mapW[ipy_add_yo * mapX + ipx_add_xo] = 0;
             }
-        }
-    }
 
-    //---RENDER MAP----
-
-    private int mapX = 8, mapY = 8, mapS = 64;
-
-    int mapW[] = {
-            2, 2, 2, 2, 2, 2, 2, 2,
-            2, 0, 0, 0, 2, 0, 0, 2,
-            2, 0, 0, 0, 4, 0, 0, 3,
-            2, 2, 4, 2, 2, 2, 4, 2,
-            2, 0, 0, 0, 0, 0, 0, 2,
-            3, 0, 0, 0, 0, 0, 0, 2,
-            2, 0, 0, 0, 0, 0, 0, 2,
-            2, 2, 2, 2, 2, 2, 2, 2
-    };
-
-    int mapF[] = {
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 1, 0, 0, 0,
-            0, 0, 0, 1, 0, 0, 0, 0,
-            0, 0, 0, 1, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 1, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0
-    };
-
-    int mapC[] = {
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0
-    };
-
-    void drawMap2D() {
-        int y, x, yo, xo;
-        for (y = 0; y < mapY; y++) {
-            for (x = 0; x < mapX; x++) {
-                if (mapW[y * mapX + x] > 0) {
-                    glColor3f(1, 1, 1);
-                } else glColor3f(0, 0, 0);
-                xo = x * mapS;
-                yo = y * mapS;
-
-                glBegin(GL_QUADS);
-                glVertex2i(xo + 1, yo + 1);
-                glVertex2i(xo + 1, yo + mapS - 1);
-                glVertex2i(xo + mapS - 1, yo + mapS - 1);
-                glVertex2i(xo + mapS - 1, yo + 1);
-                glEnd();
-            }
+//            if (mapW[ipy_add_yo * mapX + ipx_add_xo] == 7) {
+//                mapW = new int[]{
+//                        2, 2, 7, 2, 2, 5, 2, 2,
+//                        6, 0, 0, 0, 0, 0, 0, 2,
+//                        2, 0, 0, 0, 0, 0, 0, 3,
+//                        2, 0, 0, 0, 0, 2, 4, 2,
+//                        2, 0, 0, 0, 0, 0, 0, 2,
+//                        3, 0, 0, 0, 0, 0, 0, 3,
+//                        2, 0, 0, 0, 0, 0, 0, 2,
+//                        2, 2, 2, 3, 6, 2, 2, 2
+//                };
+//            }
         }
     }
 }
--- src/WindowMain.java
+++ src/WindowMain.java
@@ -3,50 +3,63 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
+import java.awt.*;
+
 import static org.lwjgl.opengl.GL11.*;
 
 public class WindowMain {
 
-    private int width = 1024;
-    private int height = 512;
+    private final int width = 955;
+    private final int height = 640;
 
     //-----FPS-----
     private long lastTime = System.nanoTime();
     private double delta = 0.0;
-    private double ns = 1000000000.0 / 60.0;
+    private final double ns = 1000000000.0 / 60.0;
     private long timer = System.currentTimeMillis();
     private int frames = 0;
-    public int getFrames() {
-        return frames;
-    }
-    public void run(){
-        try{
+
+    public void run() {
+        try {
             Display.setDisplayMode(new DisplayMode(width, height));
             Display.setTitle("Hello World");
             Display.create();
-        } catch(LWJGLException e){
+        } catch (LWJGLException e) {
             e.printStackTrace();
         }
 
-        //Initiaize code OpenGL
-        glClearColor(0.3f,0.3f,0.3f,1);
+        //TEXTURE Initialization
+        Textures textures = new Textures();
+        Textures.houseWall = textures.readFileTexture("src/Textures/Wall.csv");
+        Textures.houseDoor = textures.readFileTexture("src/Textures/Door.csv");
+        Textures.houseWindow = textures.readFileTexture("src/Textures/Window.csv");
+        Textures.houseCeiling = textures.readFileTexture("src/Textures/Ceiling.csv");
+        Textures.houseFloor = textures.readFileTexture("src/Textures/Floor.csv");
+        Textures.houseDecoOne = textures.readFileTexture("src/Textures/Wall_deco_1.csv");
+        Textures.houseDecoTwo = textures.readFileTexture("src/Textures/Wall_deco_2.csv");
+        Textures.Missing_Texture = textures.readFileTexture("src/Textures/Missing_Texture.csv");
+        //TEXTURES
+
+        //Initialize code OpenGL
+        glClearColor(0.3f, 0.3f, 0.3f, 1);
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        glOrtho(0, width, height,0,1.0f,-1.0f);
+        glOrtho(0, width, height, 0, 1.0f, -1.0f);
         glMatrixMode(GL_MODELVIEW);
+
 
         Render render = new Render();
         float pa = render.getPa();
-        render.setData(((float)Math.cos(pa)*5), ((float)Math.sin(pa)*5), 300, 300);
+        render.setData(((float) Math.cos(pa) * 5), ((float) Math.sin(pa) * 5), 300, 300);
 
         //render loop
-        while(!Display.isCloseRequested()){
+        while (!Display.isCloseRequested()) {
             //-----FPS-----
             long now = System.nanoTime();
-            delta += ( now - lastTime) / ns;
+            delta += (now - lastTime) / ns;
             lastTime = now;
 
-            if (delta >= 1.0){
+            if (delta >= 1.0) {
                 delta--;
             }
 
@@ -56,20 +69,20 @@ public class WindowMain {
             frames++;
 
             //-----FPS-----
-            if (System.currentTimeMillis() - timer > 1000){
-                timer+=1000;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
                 System.out.println(frames + " fps");
                 frames = 0;
             }
 
-            if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)){
+            if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
                 Display.destroy();
                 System.exit(0);
             }
 
             Display.setResizable(false);
             Display.update();
-            Display.sync(30);
+            Display.sync(60);
         }
 
         Display.destroy();
